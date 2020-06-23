bedc112cdbb5ab439293ef8576f01e6cb628343d
bruno-sam <30966168+bruno-sam@users.noreply.github.com>
2020-06-23 15:04:11 -0300

    Tabulacao

--- RestWithSpringBoot 02/src/main/java/br/com/bruno/exception/ExceptionResponse.java
+++ RestWithSpringBoot 02/src/main/java/br/com/bruno/exception/ExceptionResponse.java
@@ -6,9 +6,9 @@ import java.util.Date;
 public class ExceptionResponse implements Serializable {
 	
 	private static final long serialVersionID = 1L;
-    private Date timestamp;
-    private String message;
-    private String details;
+        private Date timestamp;
+        private String message;
+        private String details;
 	public ExceptionResponse(Date timestamp, String message, String details) {
 		super();
 		this.timestamp = timestamp;
